2839a2f7ef3e46cca13ec96b5acc9b7126e3fa59
Oleksandr <sasha.klishch1@gmail.com>
2025-02-16 12:52:07 +0200

    Create variant1.cpp

---
+++ variant1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float a,b,xp,xk,h,x,y ;
+    printf("a = "); scanf("%f",&a);
+    printf("b = "); scanf("%f",&b);
+    printf("xp = "); scanf("%f",&xp);
+    printf("xk = "); scanf("%f",&xk);
+    printf("h = "); scanf("%f",&h);
+    x=xp;
+    while(x<=xk+h/2){
+        y=tan(x*a)/asin(0.05*a*x)+exp(3.7)*log10(b*b)-pow(b*x,1/3);
+        printf("x = %f \t \t y = %f \n",x,y);
+        x+=h;
+    }
+    
+    return 0;
+}
